adb1ed58b96a887783c246fb9c097937f7a7efb0
Иван Шабанов <coder10@web-axioma.ru>
2021-12-08 14:32:49 +0700

    Html parser - next ver

--- class/html.parser.class.php
+++ class/html.parser.class.php
@@ -25,105 +25,66 @@ class CHtmlParser {
         /* images */
         $result['images'] = $this->get_tags('img', $content, false);
 
-        /* images */
-        $result['pictures'] = $this->get_tags('picture', $content, false);
+        /* pictures */
+        $result['pictures'] = $this->get_tags('picture', $content);
+        if (is_array($result['pictures'])) {
+            foreach ($result['pictures'] as $k=>$pic) {
+                $result['pictures'][$k]['source'] = $this->get_tags('source', $pic['text'], false);
+                $result['pictures'][$k]['img'] = $this->get_tags('img', $pic['text'], false);
+            }
+        }
+
+        /* video */
+        $result['video'] = $this->get_tags('video', $content);
+        if (is_array($result['video'])) {
+            foreach ($result['video'] as $k=>$video) {
+                $result['video'][$k]['source'] = $this->get_tags('source', $video['text'], false);
+            }
+        }
+
+        /* scripts */
+        $result['scripts'] = $this->get_tags('script', $content);
 
         /* forms */
-        $result['forms'] = $this->get_tags('form', $content, false);
+        $result['forms'] = $this->get_tags('form', $content);
 
         return $result;
     }
 
 
-    function get_tags($tag, $content, $haveclosedtag = true) {
-        if ($haveclosedtag) {
-            $arTag['tag'] = '/(<'.$tag.'[^>]*>)(.*)<\/'.$tag.'>/i';;
+    function get_tags($tag, $content, $haveClosedTag = true) {
+        if ($haveClosedTag) {
+            $arTag['tag'] = '/(<'.$tag.'[^>]*>)(.*)<\/'.$tag.'>/ismuU';;
         } else {
-            $arTag['tag'] = '/(<'.$tag.'[^>]*>)/i';
-        }
-        $arTag['attr'] = '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i';
-        /*
-        $arTags['meta'] = array(
-            'tag' => '/(<meta[^>]*>)/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['link'] = array(
-            'tag' => '/(<link[^>]*>)/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['title'] = array(
-            'tag' => '/(<title[^>]*>)([^<]+)<\/title>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['h1'] = array(
-            'tag' => '/(<h1[^>]*>)([^<]+)<\/h1>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['h2'] = array(
-            'tag' => '/(<h2[^>]*>)([^<]+)<\/h2>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['h3'] = array(
-            'tag' => '/(<h3[^>]*>)([^<]+)<\/h3>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['h4'] = array(
-            'tag' => '/(<h4[^>]*>)([^<]+)<\/h4>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['h5'] = array(
-            'tag' => '/(<h5[^>]*>)([^<]+)<\/h5>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['h6'] = array(
-            'tag' => '/(<h6[^>]*>)([^<]+)<\/h6>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['a'] = array(
-            'tag' => '/(<a[^>]*>)([^<]+)<\/a>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-
-        $arTags['img'] = array(
-            'tag' => '/(<img[^>]*>)/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['form'] = array(
-            'tag' => '/<form[^>]*>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        $arTags['script'] = array(
-            'tag' => '/<script[^>]*>/i',
-            'attr'=> '/\s+([a-zA-Z-]+)\s*=\s*("[^"]*")/i'
-        );
-        */
+            $arTag['tag'] = '/(<'.$tag.'[^>]*>)/ismuU';
+        };
+        $arTag['attr'] = '/\s+([a-zA-Z-]+)\s*=\s*"([^"]*)"/ismuU';
+        $arTag['attr2'] = str_replace('"', "'", $arTag['attr']);
         $result = array();
-            if (preg_match_all($arTag['tag'], $content, $matches)) {
-                foreach ($matches[0] as $k=>$match) {
-                    $res_tag = array();
-                    $res_tag['tag'] = $match;
-                    if (isset($matches[1][$k]))  {
-                        preg_match_all($arTag['attr'], $matches[1][$k], $attr_matches);
-                        if (is_array($attr_matches[1])) {
-                            foreach ($attr_matches[1] as $key=>$val) {
-                                $res_tag[$val] = trim($attr_matches[2][$key], '"');
-                            };
-                        }
-                        $arTag['attr2'] = str_replace('"', "'", $arTag['attr']);
-                        preg_match_all($arTag['attr2'], $matches[1][$k], $attr_matches2);
-                        if (is_array($attr_matches2[1])) {
-                            foreach ($attr_matches2[1] as $key=>$val) {
-                                $res_tag[$val] = trim($attr_matches[2][$key], '"');
-                            };
+        if (preg_match_all($arTag['tag'], $content, $matches)) {
+            foreach ($matches[0] as $k=>$match) {
+                $res_tag = array();
+                $res_tag['tag'] = $match;
+                if (isset($matches[1][$k]))  {
+                    preg_match_all($arTag['attr'], $matches[1][$k], $attr_matches);
+                    if (is_array($attr_matches[1])) {
+                        foreach ($attr_matches[1] as $key=>$val) {
+                            $res_tag[$val] = $attr_matches[2][$key];
+                        };
+                    }
+                    preg_match_all($arTag['attr2'], $matches[1][$k], $attr_matches2);
+                    if (is_array($attr_matches2[1])) {
+                        foreach ($attr_matches2[1] as $key=>$val) {
+                            $res_tag[$val] = $attr_matches[2][$key];
                         };
                     };
-                    if (isset($matches[2][$k])) {
-                        $res_tag['text'] = $matches[2][$k];
-                    };
-                    $result[] = $res_tag;
                 };
+                if (isset($matches[2][$k])) {
+                    $res_tag['text'] = $matches[2][$k];
+                };
+                $result[] = $res_tag;
             };
-
+        };
         return $result;
     }
 }
