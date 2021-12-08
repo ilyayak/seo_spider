790e43e1a23d6807e4194839f5bfb0047e147c9c
Иван Шабанов <coder10@web-axioma.ru>
2021-12-08 12:11:02 +0700

    todo отдельно

--- class/site.parser.class.php
+++ class/site.parser.class.php
@@ -1,46 +1,7 @@
 <?php
-
-/*
-todo:
-- Параметры сканирования
-- Тесты на существование
-    - Favicons
-    - OpenGraph
-    - title
-    - description
-    - canonical
-    - h1
-    - ссылки на страницу в sitemap.xml
-    - YM
-    - GA
-    - meta viewport
-- Тесты на уникальность
-    - title
-    - description
-    - content
-    - h1
-- Тесты на проиводительность
-    - кол-во и сжатость css
-    - кол-во и сжатость js
-    - где расположены css и js
-    - размеры контента/картинок
-- Тест на lazyload
-- Тест на ошибки в верстке (https://validator.w3.org/)
-
-
-- Тест на htpp / https, должен правильно редиректить
-- Тест если добавлять удалять слеши с конце
-- Тест если добвить в конец случайные символы должен вернуть 404
-- Тест микроразметка Хлебных крошек
-- Использование webp
-- Тест на Last Modified и If-Modified-Since
-- ссылки на внешние ресурсы с target="_blacnk" rel="nofollow"
-- Тест есть ли формы на всех страницах
-*/
 include_once('html.parser.class.php');
 include_once('sitemap.parser.class.php');
 include_once('robots.parser.class.php');
-
 class siteparser
 {
     var $site = '',
