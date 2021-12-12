af8abbf35c05fddb8c928de9eb75b7f0d3c67397
Ivan Shabanov <ivan.shabanov@gmail.com>
2021-12-12 17:35:26 +0700

    Fix bug html parser

--- class/html.parser.class.php
+++ class/html.parser.class.php
@@ -58,24 +58,21 @@ class CHtmlParser {
         } else {
             $arTag['tag'] = '/(<'.$tag.'[^>]*>)/ismuU';
         };
-        $arTag['attr'] = '/\s+([a-zA-Z-]+)\s*=\s*"([^"]*)"/ismuU';
-        $arTag['attr2'] = str_replace('"', "'", $arTag['attr']);
+        $arTag['attr'][0] = '/\s+([a-zA-Z-]+)\s*=\s*"([^"]*)"/ismuU';
+        $arTag['attr'][] = str_replace('"', "'", $arTag['attr'][0]);
         $result = array();
         if (preg_match_all($arTag['tag'], $content, $matches)) {
             foreach ($matches[0] as $k=>$match) {
                 $res_tag = array();
                 $res_tag['tag'] = $match;
                 if (isset($matches[1][$k]))  {
-                    preg_match_all($arTag['attr'], $matches[1][$k], $attr_matches);
-                    if (is_array($attr_matches[1])) {
-                        foreach ($attr_matches[1] as $key=>$val) {
-                            $res_tag[$val] = $attr_matches[2][$key];
-                        };
-                    }
-                    preg_match_all($arTag['attr2'], $matches[1][$k], $attr_matches2);
-                    if (is_array($attr_matches2[1])) {
-                        foreach ($attr_matches2[1] as $key=>$val) {
-                            $res_tag[$val] = $attr_matches[2][$key];
+                    foreach ($arTag['attr'] as $arTagAttr) {
+                        unset($attr_matches);
+                        preg_match_all($arTagAttr, $matches[1][$k], $attr_matches);
+                        if (is_array($attr_matches[1])) {
+                            foreach ($attr_matches[1] as $key=>$val) {
+                                $res_tag[$val] = $attr_matches[2][$key];
+                            };
                         };
                     };
                 };
